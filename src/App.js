fef63889afc014caefda3fd97ff7e39c0ee64a13
Vaibhav Gupta <guptavaibhav432@gmail.com>
2021-09-24 14:58:41 +0530

    Signin Signup and Dashboard

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
 import React from "react";
+import SignIn from "./Components/SignIn";
+import Checkout from "./Components/SignUp/Checkout";
+import Dashboard from "./Components/Dashboard/Dashboard";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 const App = () => {
-  return <div></div>;
+  return (
+    <Router>
+      <Switch>
+        <Route key="signin" path="/signin/farmer">
+          <SignIn />
+        </Route>
+        <Route key="signup" path="/signup/farmer">
+          <Checkout />
+        </Route>
+        <Route key="dashboard" path="/dashboard/farmer">
+          <Dashboard />
+        </Route>
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
