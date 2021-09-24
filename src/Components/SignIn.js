77b55a9cc870b44524daa9fdc6a868d3ac58b986
Vaibhav Gupta <guptavaibhav432@gmail.com>
2021-09-24 15:15:49 +0530

    bg

--- src/Components/SignIn.js
+++ src/Components/SignIn.js
@@ -3,22 +3,20 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Link } from "react-router-dom";
 
 function Copyright(props) {
   return (
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
       {'Copyright © '}
       <Link color="inherit" href="https://material-ui.com/">
-        Your Website
+        Farmactors
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
@@ -49,7 +47,7 @@ const SignIn = ()=> {
           sm={4}
           md={7}
           sx={{
-            backgroundImage: 'url(https://source.unsplash.com/random)',
+            backgroundImage: 'url(https://source.unsplash.com/1600x900/?farm,green)',
             backgroundRepeat: 'no-repeat',
             backgroundColor: (t) =>
               t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
@@ -70,6 +68,9 @@ const SignIn = ()=> {
             <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
               <LockOutlinedIcon />
             </Avatar>
+            <Typography component="h1" variant="h4">
+              Farmactors
+            </Typography>
             <Typography component="h1" variant="h5">
               Sign in
             </Typography>
@@ -78,10 +79,10 @@ const SignIn = ()=> {
                 margin="normal"
                 required
                 fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
+                id="username"
+                label="Username "
+                name="username"
+                autoComplete="username"
                 autoFocus
               />
               <TextField
@@ -94,10 +95,6 @@ const SignIn = ()=> {
                 id="password"
                 autoComplete="current-password"
               />
-              <FormControlLabel
-                control={<Checkbox value="remember" color="primary" />}
-                label="Remember me"
-              />
               <Button
                 type="submit"
                 fullWidth
@@ -107,13 +104,8 @@ const SignIn = ()=> {
                 Sign In
               </Button>
               <Grid container>
-                <Grid item xs>
-                  <Link href="#" variant="body2">
-                    Forgot password?
-                  </Link>
-                </Grid>
                 <Grid item>
-                  <Link href="#" variant="body2">
+                  <Link to="/signup/farmer" variant="body2">
                     {"Don't have an account? Sign Up"}
                   </Link>
                 </Grid>
