cd7b5c25b63b2316814263cddfe64376e76126c0
AdityaTeltia <teltia.aditya22@gmail.com>
2021-09-24 21:18:05 -0700

    Added card in buy
    Signed-off-by: AdityaTeltia <teltia.aditya22@gmail.com>

--- src/pages/Buy/index.js
+++ src/pages/Buy/index.js
@@ -1,7 +1,26 @@
 import React from "react";
+import Grid from "@mui/material/Grid";
+import Card from "./Card.js";
+import { useMediaQuery } from "react-responsive";
 
 const Index = () => {
-  return <div>Buy Things</div>;
+  const isPC1 = useMediaQuery({
+    query: "(max-width: 650px)",
+  });
+  const isPC2 = useMediaQuery({
+    query: "(max-width: 415px)",
+  });
+  return (
+    <div className="main-container">
+      <div className="cards" style={{ marginTop: "5%" }}>
+        <Grid container spacing={2}>
+          <Grid item xs={isPC2 ? 12 : isPC1 ? 6 : 4}>
+            <Card />
+          </Grid>
+        </Grid>
+      </div>
+    </div>
+  );
 };
 
 export default Index;
